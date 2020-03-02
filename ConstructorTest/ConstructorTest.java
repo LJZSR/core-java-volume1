42e067316e098d9fb2ea30c1959cbfc5919d63c4
LJZSR <43992884+LJZSR@users.noreply.github.com>
2020-03-02 22:37:55 +0800

    4-5

---
+++ ConstructorTest/ConstructorTest.java
@@ -0,0 +1,63 @@
+package ConstructorTest;
+
+import java.util.*;
+
+public class ConstructorTest {
+  public static void main(String[] args) {
+    Employee[] staff = new Employee[3];
+
+    staff[0] = new Employee("Harry", 40000); 
+    staff[1] = new Employee(60000);
+    staff[2] = new Employee();
+
+    for (Employee e : staff) {
+      System.out.println("name: " + e.getName() + ", salary: " + e.getSalary() + ", id:" + e.getId());
+    }
+  }
+}
+
+class Employee {
+
+  private static int nextId;
+
+  private int id;
+  private String name = "";
+  private double salary;
+
+  static {
+    Random generator = new Random();
+    nextId = generator.nextInt(1000);
+  }
+  
+  {
+    id = nextId;
+    ++nextId;
+    System.out.println(nextId);
+  }
+
+  public Employee(String name, double salary) {
+    this.name = name;
+    this.salary = salary;
+    System.out.println(name + " " + nextId);
+  }
+
+  public Employee(double salary) {
+    this("Employee# " + nextId, salary);
+  }
+
+  public Employee() {
+    System.out.println(nextId);
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public double getSalary() {
+    return salary;
+  }
+
+  public int getId() {
+    return id;
+  }
+}
