bc863377a0a07589e4e81def0ac194496ba87707
LJZSR <43992884+LJZSR@users.noreply.github.com>
2020-03-03 20:36:19 +0800

    5-8 5-9 5-10

---
+++ equals/Manager.java
@@ -0,0 +1,39 @@
+package equals;
+
+import java.util.Objects;
+
+public class Manager extends Employee {
+
+  private double bonus;
+
+  public Manager(String name, double salary, int year, int month, int day) {
+    super(name, salary, year, month, day);
+    bonus = 0;
+  }
+
+  @Override
+  public double getSalary() {
+    return super.getSalary() + bonus;
+  }
+
+  public void setBonus(double bonus) {
+    this.bonus = bonus;
+  }
+
+  @Override
+  public boolean equals(Object otherObject) {
+    if (!super.equals(otherObject)) return false;
+    Manager other = (Manager)otherObject;
+    return bonus == other.bonus;
+  }
+
+  @Override 
+  public int hashCode() {
+    return super.hashCode() + Objects.hash(bonus);
+  }
+
+  @Override 
+  public String toString() {
+    return super.toString() + "[bonus = " + bonus + "]";
+  }
+}
