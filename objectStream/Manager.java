db9adb79b2431abda2f59941f93da48ae26f66c5
LJZSR <43992884+LJZSR@users.noreply.github.com>
2020-03-24 22:16:46 +0800

    2-3

---
+++ objectStream/Manager.java
@@ -0,0 +1,17 @@
+package objectStream;
+
+public class Manager extends Employee {
+  private Employee secretary;
+
+  public Manager (String name, double salary, int year, int month, int day) {
+    super(name, salary, year, month, day);
+  }
+
+  public void setSecretary(Employee secretary) {
+    this.secretary = secretary;
+  }
+
+  public String getSecretary() {
+    return secretary.getName();
+  }
+}
