7ca6cecb922811c0947d7348ee34f690e17ffdaf
LJZSR <43992884+LJZSR@users.noreply.github.com>
2020-03-24 16:07:18 +0800

    2-1

---
+++ textFile/Employee.java
@@ -0,0 +1,27 @@
+package textFile;
+
+import java.time.LocalDate;
+
+public class Employee {
+  private String name;
+  private double salary;
+  private LocalDate hireDate;
+
+  public Employee(String name, double salary, int year, int month, int day) {
+    this.name = name;
+    this.salary = salary;
+    this.hireDate = LocalDate.of(year, month, day);
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public double getSalary() {
+    return salary;
+  }
+
+  public LocalDate getHireDay() {
+    return hireDate;
+  }
+}
